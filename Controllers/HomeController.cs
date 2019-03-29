8f0096c120d392e2d8c8b43688b14ec6c24369d3
Eduardo J. Nascimento <eduardojdn1997@gmail.com>
2019-03-29 17:08:16 -0300

    First MVC Department.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using DotNetCoreWebMVC.Models;
+using DotNetCoreWebMVC.Models.ViewModels;
 
 namespace DotNetCoreWebMVC.Controllers
 {
