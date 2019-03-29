38ff6950d5ebff18146df9569d0fd375857bc7eb
Eduardo J. Nascimento <eduardojdn1997@gmail.com>
2019-03-29 17:58:45 -0300

    Changing theme of Bootstrap;

--- Startup.cs
+++ Startup.cs
@@ -37,7 +37,7 @@ namespace DotNetCoreWebMVC
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
             services.AddDbContext<DotNetCoreWebMVCContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("DotNetCoreWebMVCContext")));
+                    options.UseNpgsql(Configuration.GetConnectionString("DotNetCoreWebMVCContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
