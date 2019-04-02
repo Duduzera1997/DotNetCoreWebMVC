f9d79ca117f1d21a9f4188e4f434bd7fc7c1b473
Duduzera1997 <eduardojdn1997@gmail.com>
2019-04-02 12:03:48 -0300

    Implementing Simple Search.

---
+++ Services/SalesRecordService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using DotNetCoreWebMVC.Models;
+
+namespace DotNetCoreWebMVC.Services
+{
+    public class SalesRecordService
+    {
+        private readonly DotNetCoreWebMVCContext _context;
+
+
+        // Construtor com injeção de dependência.
+        public SalesRecordService(DotNetCoreWebMVCContext context)
+        {
+            this._context = context;
+        }
+
+        public async Task<List<SalesRecord>> FindByDateAsync(DateTime? minDate, DateTime? maxDate)
+        {
+            var result = from salesRecord in _context.SalesRecord select salesRecord;
+            if (minDate.HasValue)
+            {
+                result = result.Where(x => x.Date >= minDate.Value);
+            }
+            if (maxDate.HasValue)
+            {
+                result = result.Where(x => x.Date <= maxDate.Value);
+
+            }
+
+            return await result.Include(x => x.Seller).Include(x => x.Seller.Department).OrderByDescending(x => x.Date).ToListAsync();
+
+        }
+    }
+}
