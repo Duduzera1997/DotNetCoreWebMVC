0c359377f34fe3ed6d1c1bf886e477f1010db20b
Duduzera1997 <eduardojdn1997@gmail.com>
2019-04-02 14:20:32 -0300

    Implementing Grouping Search & Finished CRUD Project.

--- Models/SalesRecord.cs
+++ Models/SalesRecord.cs
@@ -1,4 +1,5 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 using DotNetCoreWebMVC.Models.Enums;
 
 
@@ -7,7 +8,12 @@ namespace DotNetCoreWebMVC.Models
     public class SalesRecord
     {
         public int Id { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime Date { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:F2}")]
         public double Amount { get; set; }
         public SaleStatus Status { get; set; }
         public Seller Seller { get; set; }
